f27401805cff496904c54f55f2a19609f7fe8c62
HunterFlechsig <hflechsi@asu.edu>
2025-04-08 18:57:24 -0700

    shad-cn components and chnages to main page

---
+++ save-a-card/src/lib/searchCard.ts
@@ -0,0 +1,35 @@
+import { CardModel } from "@/models/card";
+
+export async function searchCard(name: string, set: string) {
+
+    const url = `https://api.pokemontcg.io/v2/cards?q=set.name:${set}* name:${name}*&select=name,set,images`
+
+    try {
+        const response = await fetch(url, {
+          headers: {
+            'X-Api-Key': process.env.PTCG_KEY || '',
+          }
+        });
+    
+        if (!response.ok) {
+          throw new Error(`API error: ${response.status}`);
+        }
+    
+        const { data } = await response.json();
+    
+        if (!Array.isArray(data)) {
+          throw new Error('Unexpected response structure');
+        }
+
+
+        return data.map((card): CardModel => ({
+            id: card.id,
+            name: card.name || 'Unknown Name',
+            image: card.images?.small || null,
+            setName: card.set?.name || 'Unknown Set',
+        }));
+      } catch (err) {
+        console.error('Failed to fetch or parse cards:', err);
+        return [];
+      }
+}
