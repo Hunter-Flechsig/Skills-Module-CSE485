de2419d15f2bfbeb385557ef910374b492749dbb
HunterFlechsig <hflechsi@asu.edu>
2025-04-08 20:32:48 -0700

    create adding pokemon and getting from redis - added pokemon actions, get and add - added redis connection - added card model

---
+++ save-a-card/src/app/db/redis.ts
@@ -0,0 +1,14 @@
+import { createClient } from 'redis';
+
+export const client = createClient({
+    username: 'default',
+    password: 'LdWl5MNxhOQ4kKRJJXWoA4Lgwh8ZqU1m',
+    socket: {
+        host: 'redis-18204.c270.us-east-1-3.ec2.redns.redis-cloud.com',
+        port: 18204
+    }
+});
+
+client.on('error', err => console.log('Redis Client Error', err));
+
+
